68614f062c672c2afd32a9a75250c0549f48e672
Erik Dzotsenidze <erdzot@ttu.ee>
2020-10-08 14:20:55 +0300

    Add basic landing page

---
+++ src/views/Landing.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="text-center">
+        <h1>{{ $t("title") }}</h1>
+        <p>{{$t("subtitle")}}</p>
+        <v-btn to="/reference">View all references</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Landing"
+};
+</script>
+
+<style scoped></style>

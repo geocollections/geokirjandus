6a6d4251d7a51b49b5bc45a30dd297644b9e38c2
Erikdzo <erikdzotsenidze@gmail.com>
2020-10-15 14:45:39 +0300

    Add marker clustering and different base maps

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -4,7 +4,11 @@
 
 <script>
 import "leaflet/dist/leaflet.css";
+
 import L from "leaflet";
+import "leaflet.markercluster";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -12,6 +16,7 @@ L.Icon.Default.mergeOptions({
   iconUrl: require("leaflet/dist/images/marker-icon.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png")
 });
+
 export default {
   name: "LeafletMap",
   props: {
@@ -21,15 +26,119 @@ export default {
   },
   data() {
     return {
-      map: null
+      map: null,
+      maps: [
+        {
+          name: "CartoDB",
+          leafletObject: L.tileLayer(
+            "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+            {
+              attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            }
+          ),
+          minZoom: 1,
+          maxZoom: 18
+        },
+        {
+          name: "OpenStreetMap",
+          leafletObject: L.tileLayer(
+            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            {
+              attribution:
+                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }
+          ),
+          minZoom: 1,
+          maxZoom: 18
+        },
+        {
+          name: "OpenTopoMap",
+          leafletObject: L.tileLayer(
+            "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+            {
+              attribution:
+                'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+            }
+          ),
+          minZoom: 1,
+          maxZoom: 18
+        },
+        {
+          name: "Estonian satellite",
+          leafletObject: L.tileLayer(
+            "https://tiles.maaamet.ee/tm/tms/1.0.0/foto@GMC/{z}/{x}/{-y}.png&ASUTUS=TALTECH&KESKKOND=LIVE&IS=SARV",
+            {
+              attribution:
+                "Estonian maps: <a  href='http://www.maaamet.ee/'>Republic of Estonia Land Board</a>",
+              tms: true,
+              worldCopyJump: true,
+              detectRetina: true,
+              zIndex: 1,
+              updateWhenIdle: true,
+              continuousWorld: true
+            }
+          ),
+          minZoom: 6,
+          maxZoom: 18
+        },
+        {
+          name: "Estonian map",
+          leafletObject: L.tileLayer(
+            "https://tiles.maaamet.ee/tm/tms/1.0.0/kaart@GMC/{z}/{x}/{-y}.png&ASUTUS=TALTECH&KESKKOND=LIVE&IS=SARV",
+            {
+              attribution:
+                "Estonian maps: <a  href='http://www.maaamet.ee/'>Republic of Estonia Land Board</a>",
+              tms: true,
+              worldCopyJump: true,
+              detectRetina: true,
+              zIndex: 1,
+              updateWhenIdle: true,
+              continuousWorld: true
+            }
+          ),
+          minZoom: 6,
+          maxZoom: 18
+        }
+      ],
+      overlayMaps: [
+        {
+          name: "Estonian hybrid",
+          leafletObject: L.tileLayer(
+            "https://tiles.maaamet.ee/tm/tms/1.0.0/hybriid@GMC/{z}/{x}/{-y}.png&ASUTUS=TALTECH&KESKKOND=LIVE&IS=SARV",
+            {
+              attribution:
+                "Estonian maps: <a  href='http://www.maaamet.ee/'>Republic of Estonia Land Board</a>",
+              tms: true,
+              worldCopyJump: true,
+              detectRetina: true,
+              zIndex: 2,
+              updateWhenIdle: true,
+              continuousWorld: true
+            }
+          ),
+          minZoom: 6,
+          maxZoom: 18
+        }
+      ]
     };
   },
   watch: {
     markers: {
       handler() {
+        const markerClusters = L.markerClusterGroup();
+        let markers = [];
         for (const m of this.markers) {
-          L.marker(m.coordinates, { title: m.title }).addTo(this.map);
+          const markerObj = L.marker(m.coordinates, { title: m.title });
+          markers.push(markerObj);
+          if (m.popup) markerObj.bindPopup(m.popup);
+
+          markerClusters.addLayer(markerObj);
         }
+        this.map.addLayer(markerClusters);
+        let bounds = new L.featureGroup(markers).getBounds();
+        console.log(bounds);
+        this.map.fitBounds(bounds);
       }
     }
   },
@@ -38,22 +147,17 @@ export default {
   },
   methods: {
     setupLeafletMap() {
-      const mapDiv = L.map(this.$refs["mapElement"]).setView([0, 0], 1);
+      const mapDiv = L.map(this.$refs["mapElement"], {
+        layers: [this.maps[0].leafletObject]
+      }).setView([0, 0], 1);
+
+      let baseMaps = {};
+      this.maps.forEach(
+        provider => (baseMaps[provider.name] = provider.leafletObject)
+      );
+
+      L.control.layers(baseMaps).addTo(mapDiv);
       this.map = mapDiv;
-      L.tileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          minZoom: 1,
-          id: "mapbox/streets-v11",
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken:
-            "pk.eyJ1IjoiZXJpa2R6byIsImEiOiJja2VpOWIxOHowaWUyMnRwY3Nwa2RncWxsIn0.rL0RG5H3mbyCADqV24CotA"
-        }
-      ).addTo(mapDiv);
     }
   }
 };

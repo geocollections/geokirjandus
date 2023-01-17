d9479c317466753194eb6843187458db2d271fc9
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-01-17 12:17:38 +0200

    fix library references not showing

--- src/middleware/SearchService.js
+++ src/middleware/SearchService.js
@@ -108,7 +108,7 @@ function buildQueryStr(queryObject, filterQueryObject) {
               if (searchParameter.id === "volumeAndNumber") {
                 return `"${str.trim()}"`;
               }
-              return `"*${str.trim()}*"`;
+              return `"${str.trim()}"`;
             });
 
             return `${fieldId}:(${paramArray.join(" AND ")})`;

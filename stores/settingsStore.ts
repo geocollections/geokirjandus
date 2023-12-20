adf946de0e1cf1f89f25e17f7e2fa6c84366366b
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-12-20 15:53:17 +0200

    feat: add additional citation style options

--- stores/settingsStore.ts
+++ stores/settingsStore.ts
@@ -5,6 +5,13 @@ export const useSettingsStore = defineStore(
       { value: "apa", name: "APA" },
       { value: "harvard1", name: "Harvard" },
       { value: "sedimentology", name: "Sedimentology" },
+      { value: "geol-mag", name: "Geological magazine" },
+      { value: "holocene", name: "Holocene" },
+      { value: "nature", name: "Nature" },
+      { value: "geology", name: "Geology" },
+      { value: "palaeo3", name: "Palaeo3" },
+      { value: "ejes", name: "EJES" },
+      { value: "gsl", name: "The Geological Society of London" },
     ]);
     const citationStyle = ref(citationStyleOptions.value[0].value);
 
